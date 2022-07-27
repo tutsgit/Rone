b86843a164a9c9c46fabf0edfa00662beffa0b49
tutsgit <tutsgit@gmail.com>
2022-07-28 00:18:48 +0100

    fix map function

--- src/Components/App/LINK/Links.jsx
+++ src/Components/App/LINK/Links.jsx
@@ -1,10 +1,12 @@
 import './Links.css';
-export default function Links({id, name, age}) {
+export default function Links({arr}) {
     return(
-        <div>
-            <h1>{name}</h1>
-            <h5>{age}</h5>
-            <h6>{id}</h6>
-        </div>
+        <>
+            {
+            arr.map(({id, name, age}, idx) => (
+                <h5>{name}</h5>
+            ))
+        }
+        </>
     )
 }
--- src/Components/App/Nav/Nav.jsx
+++ src/Components/App/Nav/Nav.jsx
@@ -1,26 +1,14 @@
 import './Nav.css'
 import Links from '../LINK/Links';
 export default function Nav() {
-    const links_arr = [
-        {
-            id: 1,
-            name: 'Aaaaa',
-            age: 25
-        },
-        {
-            id: 2,
-            name: 'Ammmm',
-            age: 25
-        }
+    const _arr = [
+        {id: 1,name: 'Aaaaa',age: 25},
+        {id: 2,name: 'Ammmm',age: 25}
     ];
     return (
         <div className="nav">
-            <h1>Company Name</h1>
-            {
-                links_arr.map((lnk, i) => (
-                    <Links arr = {lnk} key={i}/>
-                ))
-            }
+            <h1>Am Company</h1>
+            <Links arr = {_arr}/>
         </div>
     )
 }
