05f2dd46066044408ba04c770cf54e3b36200179
tutsgit <tutsgit@gmail.com>
2022-07-28 01:02:29 +0100

    fix form

--- src/Components/App/Search/Search.jsx
+++ src/Components/App/Search/Search.jsx
@@ -1,13 +1,18 @@
-import './search.css';
+import "./search.css";
 export default function Search() {
+  return (
     <div className="search">
-        <form action="#">
-            <div className="input">
-                <input type="text" name="searchText" />
-            </div>
-            <div className="btn">
-                <input type="submit" value="Submit" />
-            </div>
-        </form>
+      <form action="#">
+        <div className="input">
+          <input type="text" name="searchText" />
+        </div>
+        <div className="btn">
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+      <div className="res">
+        <h5>data</h5>
+      </div>
     </div>
+  );
 }
